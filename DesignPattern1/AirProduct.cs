89e278ad08125d2489c3b8af1b93ac8c4c4495c2
Neha Anand <nanand@tavisca.com>
2018-09-05 14:14:21 +0530

    Implemented version1

---
+++ DesignPattern1/AirProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern1
+{
+    class AirProduct: IProduct
+    {
+        public String GetTypeOfProduct()
+        {
+            return "Air";
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Booking Air Product");
+        }
+
+        public void Book()
+        {
+            Console.WriteLine("saving air product completed");
+        }
+    }
+}
---
+++ DesignPattern1/Factory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern1
+{
+    class Factory
+    {
+      public IProduct GetProduct(string description)
+        {
+            switch(description)
+            {
+                case "car": return new CarProduct();
+                case "air": return new AirProduct();
+                case "hotel": return new HotelProduct();
+                case "activity": return new Activity();
+                default: return new Activity();
+            }
+        }
+    }
+}
